a2dad1ac857132c772198c7efb19b26b4cb4d68f
Miguel Machado <hello@miguelmachado.dev>
2023-07-28 03:58:55 -0300

    chore: add better comments

--- router/router.go
+++ router/router.go
@@ -7,18 +7,14 @@ import (
 )
 
 func Initialize() {
-	// Initialize router
 	r := gin.Default()
 
-	// Initialize routes
 	initializeRoutes(r)
 
-	// Get the port from the environment
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
 	}
 
-	// Run the server
 	r.Run("0.0.0.0:" + port)
 }
