e83fc1e17b88262dcef944a32c4e837ab916c49d
Joao <joaodslourenco@gmail.com>
2023-08-02 05:30:15 -0300

    feat: add get team members route

--- router/routes.go
+++ router/routes.go
@@ -23,6 +23,7 @@ func initializeRoutes(r *gin.Engine) {
 	{
 		// Define a GET route for the /ping endpoint
 		v1.GET("/ping", handler.PingServerHandler)
+		v1.GET("/teams/:teamname/members", handler.ListTeamMembersHandler)
 	}
 
 	// Initialize Swagger documentation
