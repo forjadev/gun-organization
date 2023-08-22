ef51ae4bad5cde143cacf335499d22b186fe7885
Paulo Eduardo <pauloes.dev@gmail.com>
2023-08-22 19:55:22 -0300

    Moving getMembers to a method and start creating function to insertOrUpdateMember

--- jobs/profile_sync.go
+++ jobs/profile_sync.go
@@ -6,26 +6,81 @@ import (
 	"io"
 	"net/http"
 	"os"
+	"strconv"
 
 	"github.com/joho/godotenv"
+
+	"github.com/forjadev/gun-organization/config"
+	"github.com/forjadev/gun-organization/schemas"
 )
 
-type member struct {
+type Member struct {
 	Id    int    `json:"id"`
 	Login string `json:"login"`
 	URL   string `json:"url"`
 }
 
+type MemberInfo struct{}
+
+var (
+	TOKEN string
+	URL   string
+)
+
 func main() {
 	err := godotenv.Load(".env")
+	if err != nil {
+		panic(err)
+	}
+	// GITHUB
+	TOKEN = os.Getenv("GITHUB_TOKEN")
+	URL = os.Getenv("GITHUB_URL")
 
-	var TOKEN = os.Getenv("GITHUB_TOKEN")
-	var URL = os.Getenv("GITHUB_URL")
+	members, err := getMembers()
+	if err != nil {
+		panic(err)
+	}
+
+	for _, member := range members {
+		err := insertOrUpdateMember(schemas.Member{GithubID: strconv.Itoa(member.Id), Login: member.Login, URL: member.URL})
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// // FORJAA_DB
+
+	// // Initialize configs
+	// err = config.Init()
+	// if err != nil {
+	// 	panic(err)
+	// }
+
+	// db := config.GetDatabase()
+
+	// var members []schemas.Member
+	// db.Find(&members)
+
+	// fmt.Println("FIND MEMBERS")
+
+	// for _, member := range members {
+	// 	fmt.Println(member)
+	// }
+}
+
+func getMembers() ([]Member, error) {
+	err := godotenv.Load(".env")
+	if err != nil {
+		return nil, err
+	}
+	// GITHUB
+	TOKEN := os.Getenv("GITHUB_TOKEN")
+	URL := os.Getenv("GITHUB_URL")
 
 	client := &http.Client{}
 	req, err := http.NewRequest("GET", URL, nil)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
 	req.Header.Set("Accept", "application/vnd.github+json")
 	req.Header.Set("Authorization", fmt.Sprintf("Bearer %v", TOKEN))
@@ -37,14 +92,30 @@ func main() {
 	defer res.Body.Close()
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	var data []member
+	var data []Member
 	err = json.Unmarshal(body, &data)
 	if err != nil {
-		panic(err)
+		return nil, err
 	}
-	for _, member := range data {
-		fmt.Println(member)
+
+	return data, nil
+}
+
+func insertOrUpdateMember(member schemas.Member) error {
+	err := config.Init()
+	if err != nil {
+		return err
 	}
+
+	db := config.GetDatabase()
+	result := db.Create(&member)
+
+	if result.Error != nil {
+		return result.Error
+	}
+
+	fmt.Println(result.RowsAffected)
+	return nil
 }
